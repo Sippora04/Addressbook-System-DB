68fbcfb1d9f3246068e5fe77af6035718126bca2
Sippora04 <sippora.toppo95@gmail.com>
2020-11-17 22:51:58 +0530

    Added: create table for attributes

--- addressbook_system.sql
+++ addressbook_system.sql
@@ -1,3 +1,19 @@
+#UC1
 #create database Addressbook_System;
 #USE Addressbook_System;
-SELECT DATABASE();
+#SELECT DATABASE();
+
+#UC2
+CREATE TABLE address_book (
+ first_name VARCHAR(150) NOT NULL,
+ last_name 	VARCHAR(150) NOT NULL,
+ address 	VARCHAR(250) NOT NULL,
+ city 		VARCHAR(150) NOT NULL,
+ state 		VARCHAR(150) NOT NULL,
+ zip 		INT unsigned NOT NULL,
+ phone 		BIGINT  NOT NULL,
+ email 		VARCHAR(250) NOT NULL,
+ PRIMARY KEY (first_name)
+);
+
+DESCRIBE address_book;
